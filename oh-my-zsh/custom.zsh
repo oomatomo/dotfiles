0129f7272375bcfd3828a3e6e7e2446ebf531333
oomatomo <ooma0301@gmail.com>
2017-10-06 20:24:31 +0900

    [zsh] e2shの修正

--- oh-my-zsh/custom.zsh
+++ oh-my-zsh/custom.zsh
@@ -75,16 +75,20 @@ function agvi() {
   vim $(ag $@ | peco --query "$LBUFFER" | awk -F : '{print "-c " $2 " " $1}')
 }
 
-function ec2ssh() {
+function e2sh() {
   # aws-cli, jq, peco
-  hosts=(`aws ec2 describe-instances --profile $@ --filter "Name=instance-state-name,Values=running" | \
-      jq -r '.Reservations[].Instances[] | .PrivateIpAddress as $p | .Tags[] | { ip: $p, name: select(.Key == "Name").Value } | flatten | join("\t") ' | \
+  profile=${1:-default}
+  # Tgas[]? = タグが一つもの存在しないものは除外する
+  hosts=(`aws ec2 describe-instances --profile $profile --filter "Name=instance-state-name,Values=running" | \
+      jq -r '.Reservations[].Instances[] | .PrivateIpAddress as $p | .Tags[]? | { ip: $p, name: select(.Key == "Name").Value } | flatten | join("\t") ' | \
       sort -t$'\t' -k 2 | peco --query "$LBUFFER" | awk -F '\t' '{print $1}' ` )
   cnt=0
   # 一番上のパネルに移動してsshしていく
   tmux select-pane -t 0
   for host in $hosts; do
-    tmux split-window
+    if [[ $cnt -gt 0 ]]; then
+      tmux split-window
+    fi
     tmux send-keys -t $cnt "ssh octpass-$host" C-m
     tmux send-keys -t $cnt "clear" C-m
     ((cnt++))
